1bb5a9fb08a73802bf201057df9db09cda4336b7
Kireeti Chilakamarry <desichilz123@gmail.com>
2024-02-08 14:10:36 -0800

    Adding Binary Search

---
+++ BinarySearch.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+
+        while (l <= r):
+            middle = (l + r) // 2
+            if (nums[middle] == target):
+                return middle
+            elif (nums[middle] > target):
+                r = middle - 1
+            else:
+                l = middle + 1
+        return -1
